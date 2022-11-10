3df4076c2c6b03587f96c6b41cf0b2097d6a8e7c
Raaif Hadadi <hraaif@yahoo.com>
2022-11-10 22:21:40 +0000

    add api route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/api', (req, res) => {
+    res.send('Hello API!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
