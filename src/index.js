96e3a3039152e1819176355808cbe6b4f4672a3e
Raaif Hadadi <hraaif@yahoo.com>
2022-11-11 21:29:00 +0000

    [rh1320, jmh20] remove unnecessary video download

--- src/index.js
+++ src/index.js
@@ -1,53 +1,37 @@
 const express = require('express')
-const { download } = require('fetch-video');
 const axios = require('axios')
 const app = express()
 const port = 3000
-const src = ""
-const URL = require('url').URL;
 
-const queue = []
-
-const machines = []
-
-vid_request = {
-    prompt: "What is the name of the video you want to watch?",
-    setting1: "Video",
-}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/api', (req, res) => {
+app.get('/api', async (req, res) => {
 
-    // axios({
-    //     method: 'get',
-    //     url: 'http://109.158.65.154:8080/test',
-    //     responseType: 'blob',
-    //     timeout: 10000000,
-    // })
-    //     .then((response) => {
-    //         // console.log(response.data)
-    //         src = (URL.createObjectURL(response.data));
-    //         console.log(src);
-    //     })
-    // res.send(src)
 
     // axios.get('http://109.158.65.154:8080/test').then((response) => {
     //     // console.log(response);
     //     // console.log(response.data)
     //     res.status(200)
     //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.contentType('video/mp4');
     //     // res.send(response.data)
     //     res.json(response)
     // })
 
-    const downloader = download('http://109.158.65.154:8080/test', 'video.mp4', "GET")
-    downloader.go().then(() => {
-        res.sendFile(__dirname + '/video.mp4')
+    const axiosResponse = await axios({
+        url: 'http://109.158.65.154:8080/test',
+        responseType: 'stream'
     })
+
+    axiosResponse.data.pipe(res)
+    res.contentType('video/mp4');
+
+    // const downloader = download('http://109.158.65.154:8080/test', 'video.mp4', "GET")
+    // downloader.go().then(() => {
+    //     res.sendFile(__dirname + '/video.mp4')
+    // })
 
 })
 
