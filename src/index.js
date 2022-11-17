d8ba457bf3be958c6edccce9b5e79b1d79edf441
Raaif Hadadi <hraaif@yahoo.com>
2022-11-17 16:25:25 +0000

    Fix error catching. Remove unused APIs

--- src/index.js
+++ src/index.js
@@ -3,63 +3,66 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
-
+// Hello World
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+
+// API to genereate initial frames
+app.get('/api/generateFrames', (req, res) => {
+    axios.get('http://109.158.65.154:8080/test')
+        .then(response => {
+            res.send(response.data.data)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+})
+
+// Api to generate and return a video
 app.get('/generate', async (req, res) => {
 
     const query = req.query
 
-    const axiosResponse = await axios({
+    axios({
         url: 'http://109.158.65.154:8080/api',
         responseType: 'stream',
         params: query,
         timeout: 100000000
+    }).then(response => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.contentType('video/mp4');
+        response.data.pipe(res)
     }).catch((error) => {
-        res.send(error)
+        res.status(500).send(error)
     })
-
-    axiosResponse.data.pipe(res)
-    res.header("Access-Control-Allow-Origin", "*");
-    res.contentType('video/mp4');
 
 })
 
-app.get('/api', async (req, res) => {
+// *Unused* api to check server returns pre-generated video from flask fs
+app.get('/getPreGeneratedVideo', async (req, res) => {
 
-    const axiosResponse = await axios({
+    axios({
         url: 'http://109.158.65.154:8080/test',
         responseType: 'stream'
-    }).catch((error) => {
-        res.send(error)
+    }).then(response => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.contentType('video/mp4');
+        response.data.pipe(res)
+    }).catch(error => {
+        res.status(500).send(error);
     })
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.contentType('video/mp4');
-    axiosResponse.data.pipe(res)
 
 })
 
+// *Unused* api to return a pre-generated video from express fs
 app.get('/api3', async (req, res) => {
 
     res.header("Access-Control-Allow-Origin", "*");
     res.contentType('video/mp4');
     res.sendFile('./turtle.mp4', { root: __dirname });
 
-})
-
-app.get('/api2', (req, res) => {
-
-    axios.get('http://109.158.65.154:8080/test2').then((response) => {
-        // console.log(response.data)
-        res.header("Access-Control-Allow-Origin", "*");
-        // res.contentType('video/mp4')
-        res.send(response.data)
-    }).catch((error) => {
-        res.send(error)
-    })
 })
 
 app.listen(port, () => {
